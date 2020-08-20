b5c5ab6ecd29d5163947895606e5f18ef377111f
George Flores <georgeflores886@gmail.com>
2020-08-20 10:39:34 -0400

    created components for chart, cards, and country and css styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+//instead of creating multiple imports, use the index.js to import components to App.js
+import { Cards, Chart, CountryPicker} from './components'
+
+import styles from './App.module.css'
 
 class App extends React.Component {
     render() {
         return(
             <div>
-                <h1>App</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
