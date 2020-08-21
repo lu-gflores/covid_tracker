58856633b9499c45931cdb729a2b144ce5b6a237
George Flores <georgeflores886@gmail.com>
2020-08-21 15:20:24 -0400

    added media queries

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Cards, Chart, CountryPicker } from './components'
 import { fetchData } from './api'
 import styles from './App.module.css'
+import coronaImg from './images/covid_logo.png'
 
 class App extends React.Component {
     state = {
@@ -26,6 +27,7 @@ class App extends React.Component {
 
         return (
             <div className={styles.container}>
+            <img className={styles.image} src={coronaImg} alt="Covid 19"/>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart data={data} country={country} />
