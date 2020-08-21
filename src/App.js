1492ab37876ba740311c04b67dff51de662536d7
George Flores <georgeflores886@gmail.com>
2020-08-21 14:29:41 -0400

    rendered bar chart and state change

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import { fetchData } from './api'
 import styles from './App.module.css'
 
 class App extends React.Component {
-    state ={
+    state = {
         data: {},
         country: ''
     }
 
     async componentDidMount() {
         const fetchedData = await fetchData();
-        this.setState({data: fetchedData});
+        this.setState({ data: fetchedData });
     }
 
-    handleCountryChange = async(country) => {
+    handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
-        console.log(fetchedData)
-        //set state
+
+        this.setState({ data: fetchedData, country: country });
     }
 
     render() {
-        const {data} =  this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
-                <Cards data={data}/>
+                <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
